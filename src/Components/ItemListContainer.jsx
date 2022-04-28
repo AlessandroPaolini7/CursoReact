59231b6be8b075d0d0b8f9abcda4239c190bcfec
Alessandro <alepaolinicaar@gmail.com>
2022-04-28 17:58:54 -0300

    Item Collection

--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ItemList from "./ItemList";
-import traerProductos from "../util/traerProductos";
-import productos from "../util/productos";
 import { useParams } from "react-router-dom";
+import {collection, getDocs, getFirestore, query, where} from "firebase/firestore"
 
 function ItemListContainer() {
     const [items, setItems] = useState([]);
@@ -11,21 +10,33 @@ function ItemListContainer() {
 
     useEffect(()=>{
         setLoading(true);
-        traerProductos(categoryId)
-            .then(resultado => setItems(resultado))
-            .catch(error => console.log(error))
-            .finally(()=>setLoading(false));
-    },[categoryId]);
+        const db = getFirestore();
+
+        
+        if (!categoryId){
+            const itemsCollection = collection(db, "productos");
+            getDocs(itemsCollection).then(item =>{
+                setItems(item.docs.map(doc => ({id: doc.id, ...doc.data()})));
+            })
+        } 
+        else{
+            const itemsCollectionFiltered = query(collection(db, "productos"), where("category", "==", categoryId));
+            getDocs(itemsCollectionFiltered).then(item =>{
+                setItems(item.docs.map(doc => ({id: doc.id, ...doc.data()})));
+            })
+        }
+        setLoading(false);
+    }, [categoryId]);
 
     if(loading){
         return <h1>Cargando...</h1>
     }
 
-return (
+return(
     <div className="container-fluid row justify-content-around">
     <ItemList productos={items}/>
     </div>
-)
+    )
+    
 }
-
 export default ItemListContainer
--- src/index.js
+++ src/index.js
@@ -3,7 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAwJ3PhfLZFGVogVCFTpdoVUchYcx-gPG0",
+  authDomain: "electroshop-cb80f.firebaseapp.com",
+  projectId: "electroshop-cb80f",
+  storageBucket: "electroshop-cb80f.appspot.com",
+  messagingSenderId: "217193645695",
+  appId: "1:217193645695:web:56bf49caa493960444e14a",
+  measurementId: "G-KKPLJSNWQ0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 ReactDOM.render(
   <React.StrictMode>
     <App />
